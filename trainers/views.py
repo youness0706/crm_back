82fb0641e11ed268c5be59249f97cc913feb112d
youness0706 <86917405+youness0706@users.noreply.github.com>
2025-01-02 00:24:27 +0000

    Update views.py

--- trainers/views.py
+++ trainers/views.py
@@ -426,7 +426,7 @@ def delete_trainer_view(request, id):
     cat = trainer.category
     trainer.delete()
     messages.success(request, "تم حذف المدرب بنجاح.")
-    return redirect("trainees/"+cat)  # استبدل "trainers_list" بمسار القائمة الرئيسية
+    return redirect("trainees")  # استبدل "trainers_list" بمسار القائمة الرئيسية
 
 
 @login_required(login_url='/login/')
