f3dcda45777dad032935683b2925cb5a71af4159
youness0706 <86917405+youness0706@users.noreply.github.com>
2025-01-03 09:41:11 +0000

    Update settings.py edit allowed hosts

--- crm_back/settings.py
+++ crm_back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&3$#p6w4o)5)fhiv!m2powt#&%dg25^*o4yxp!at*3zb22+9m8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','nojoumargana.site]
+ALLOWED_HOSTS = ['127.0.0.1','nojoumargana.site']
 
 
 # Application definition
