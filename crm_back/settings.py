cb872fcf7660cd88283f8381f32f53990259002d
youness0706 <86917405+youness0706@users.noreply.github.com>
2025-01-08 15:56:51 +0000

    Update settings.py database ssl

--- crm_back/settings.py
+++ crm_back/settings.py
@@ -31,21 +31,6 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
-
 ALLOWED_HOSTS = ['127.0.0.1','nojoumargana.site']
 
 
@@ -98,6 +83,26 @@ WSGI_APPLICATION = 'crm_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+    # Add SSL configuration explicitly
+    DATABASES['default']['OPTIONS'] = {
+        'ssl': {
+            'ca': '/path/to/your/ca_certificate.pem',  # Use an appropriate CA cert file
+        }
+    }
 
 
 
