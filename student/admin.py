0d4523316be9fc4c09e5da2dd033a3b38b2841cb
youness0706 <younessbouhnif@gmail.com>
2025-09-20 14:56:46 +0100

    Refactor admin.py files to ensure proper model registration and remove unnecessary imports

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from student.models import *
 # Register your models here.
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(SchoolExpense)
+admin.site.register(Event)
+admin.site.register(StudentPayment)
